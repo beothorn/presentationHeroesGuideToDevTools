a976339c0f4bd36246adb984ae78f9cf716d839f
lucass <lucass@gmail.com>
2019-09-30 22:19:43 +0200

    A fake system with fake endpoints

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+Hello :)
+this is the code that simulates our system.
+It is just a giant JSON with some values inside.
+
+I just wanted to have some endpoints to call from my
+extension so don't expect to find any nice cod here :)
+
+This code is just enough to exemplify the idea
+of the presentation.
+ */
 const express = require('express');
 const app = express();
 app.use(express.static('static'));
@@ -7,46 +18,115 @@ app.use(bodyParser.json()); // support json encoded bodies
 
 const db = {
     "clients":[
-        {
-            "name": "Alice"
-        },
-        {
-            "name": "Bob"
-        },
-        {
-            "name": "Carol"
-        },
+        {"name": "Alice Restaurant"},
+        {"name": "Bob's Burguer"},
+        {"name": "Carol Soups"},
     ],
     "drivers":[
-        {
-            "name": "Dragan"
-        },
-        {
-            "name": "Edna"
-        },
+        {"name": "Dragan"},
+        {"name": "Edna"},
     ],
     "tours":[
         {
             "id": 1,
             "driver": "Dragan",
-            "finished": true,
-            "invoiced": true,
-            "date": "2019-03-25",
+            "client": "Alice Restaurant",
+            "finished": false,
+            "invoiced": false,
+            "date": "2020-02-02",
             "deliveries":[
                 {
-                    "article": "computer",
-                    "delivered": true
-                }
+                    "id": 10,
+                    "article": "potatoes",
+                    "quantity": 1,
+                    "loaded": false,
+                    "delivered": false,
+                },
+                {
+                    "id": 11,
+                    "article": "tomatoes",
+                    "quantity": 2,
+                    "loaded": false,
+                    "delivered": false,
+                },
+                {
+                    "id": 12,
+                    "article": "salt",
+                    "quantity": 1,
+                    "loaded": false,
+                    "delivered": false,
+                },
             ]
         },
         {
             "id": 2,
-            "driver": "Edna",
-            "date": "2019-03-25",
+            "driver": "Dragan",
+            "client": "Bob's Burguer",
+            "finished": false,
+            "invoiced": false,
+            "date": "2020-02-02",
             "deliveries":[
                 {
-                    "article": "desk",
-                    "delivered": false
+                    "id": 13,
+                    "article": "burguer box",
+                    "quantity": 10,
+                    "loaded": false,
+                    "delivered": false,
+                },
+                {
+                    "id": 14,
+                    "article": "buns",
+                    "quantity": 80,
+                    "loaded": false,
+                    "delivered": false,
+                },
+            ]
+        },
+        {
+            "id": 3,
+            "driver": "Edna",
+            "client": "Carol Soups",
+            "finished": false,
+            "invoiced": false,
+            "date": "2020-02-02",
+            "deliveries":[
+                {
+                    "id": 15,
+                    "article": "potatoes",
+                    "quantity": 8,
+                    "loaded": false,
+                    "delivered": false,
+                },
+                {
+                    "id": 16,
+                    "article": "carrots",
+                    "quantity": 6,
+                    "loaded": false,
+                    "delivered": false,
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "driver": "Edna",
+            "client": "Carol Soups",
+            "finished": false,
+            "invoiced": false,
+            "date": "2020-02-03",
+            "deliveries":[
+                {
+                    "id": 17,
+                    "article": "potatoes",
+                    "quantity": 3,
+                    "loaded": false,
+                    "delivered": false,
+                },
+                {
+                    "id": 18,
+                    "article": "oil",
+                    "quantity": 2,
+                    "loaded": false,
+                    "delivered": false,
                 }
             ]
         },
@@ -67,9 +147,27 @@ app.get('/api/drivers', (req, res) => {
     res.send(db.drivers)
 })
 
-app.post('/api/tours', (req, res) => {
-    console.log(req.body)
-    res.send(db.clients)
+app.get('/api/tours/:id/deliveries', (req, res) => {
+    const tourId = parseInt(req.params.id)
+    const tour = db.tours.find(t => t.id === tourId)
+    res.send(tour.deliveries)
+})
+
+const getDelivery = (id) => {
+    for(t of db.tours){
+        for(d of t.deliveries){
+            if(d.id === id){
+                return d
+            }
+        }
+    }
+}
+
+app.put('/api/deliveries/:id/loaded/:value', (req, res) => {
+    const deliveryId = parseInt(req.params.id)
+    const delivery = getDelivery(deliveryId)
+    delivery.loaded = req.params.value == "true"
+    res.status(200).end()
 })
 
 console.log(`Will run on port ${PORT}`)
